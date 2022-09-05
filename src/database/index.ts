61d84d7165a00a16104bf4d69cdaa1962af4b68e
Vlad <vladik.gmail.com>
2022-09-05 10:52:29 +0300

    db connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,22 +2,20 @@ import { ConnectionOptions } from 'typeorm';
 
 const env = process.env.NODE_ENV || 'development';
 const dbConnection: ConnectionOptions = {
-  type: 'postgres',
-  host: process.env.POSTGRESQL_HOST,
-  port: Number(process.env.POSTGRESQL_PORT),
-  username: process.env.POSTGRESQL_USERNAME,
-  password: process.env.POSTGRESQL_PASSWORD,
-  database: process.env.POSTGRESQL_DATABASE,
+  type: 'mongodb',
+  url: 'mongodb+srv://health:health@hcluster.jtitjh6.mongodb.net/?retryWrites=true&w=majority',
+  host: 'localhost',
+  port: 27017,
   synchronize: true,
   logging: false,
   entities: [env === 'production' ? 'build/entity/*{.ts,.js}' : 'src/entity/*{.ts,.js}'],
-  migrations: [env === 'production' ? 'build/migration/*{.ts,.js}' : 'src/migration/*{.ts,.js}'],
-  subscribers: [env === 'production' ? 'build/subscriber/*{.ts,.js}' : 'src/subscriber/*{.ts,.js}'],
-  cli: {
-    entitiesDir: 'src/entity',
-    migrationsDir: 'src/migration',
-    subscribersDir: 'src/subscriber',
-  },
+  // migrations: [env === 'production' ? 'build/migration/*{.ts,.js}' : 'src/migration/*{.ts,.js}'],
+  // subscribers: [env === 'production' ? 'build/subscriber/*{.ts,.js}' : 'src/subscriber/*{.ts,.js}'],
+  // cli: {
+  //   entitiesDir: 'src/entity',
+  //   migrationsDir: 'src/migration',
+  //   subscribersDir: 'src/subscriber',
+  // },
 };
 
 export { dbConnection };
